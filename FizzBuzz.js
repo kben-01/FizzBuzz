7e69eae26b377bab5f1102b12323f18a97835cb4
kobe benson <kobebenson15@gmail.com>
2020-08-27 20:58:17 -0600

    changed into a do..while loop

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,12 +1,16 @@
-for (let i = 1; i < 101; i++) {
+function fizzBuzz(i) {
     let result = i
     if (i % 3 === 0 && i % 5 === 0) {
         result = "FizzBuzz"
     } else if (i % 3 === 0) {
           result = "Fizz"
-      } else if (i % 5 === 0) {
+    } else if (i % 5 === 0){
           result = "Buzz"
-      }
-
+    }
     console.log(result)
 }
+let i = 1
+do {
+    fizzBuzz(i)
+    i++
+} while (i < 101)
